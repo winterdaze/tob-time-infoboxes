d66ee4958d091118011e666996b18067a105cfeb
winterdaze <35933468+winterdaze-rs@users.noreply.github.com>
2020-08-11 15:26:15 -0700

    Add ToB infobox plugin

--- src/test/java/com/tobinfoboxes/TobInfoboxesPluginTest.java
+++ src/test/java/com/tobinfoboxes/TobInfoboxesPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.tobinfoboxes;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class TobInfoboxesPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(TobInfoboxesPlugin.class);
 		RuneLite.main(args);
 	}
 }
